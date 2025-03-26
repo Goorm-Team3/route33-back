94160a3dc269bcc02fc8e7c5041aaa3cb1abd496
jhhong0930 <jhhong0930@aift.kr>
2025-03-26 13:17:21 +0900

    엔티티 클래스 추가

--- src/main/java/io/goorm/route33/Route33BackApplication.java
+++ src/main/java/io/goorm/route33/Route33BackApplication.java
@@ -2,7 +2,9 @@ package io.goorm.route33;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableJpaRepositories
 @SpringBootApplication
 public class Route33BackApplication {
 
